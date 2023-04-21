df1eaf8ffccd8f09be33b7ab7bc367a95646c7c0
aforozco13 <orozco.andres.f.1213@gmail.com>
2023-04-20 21:04:52 -0500

    Mi segundo commit

--- lambda_test_trigger_datasync.py
+++ lambda_test_trigger_datasync.py
@@ -1,14 +1,17 @@
 import boto3
+import sys
 
 client = boto3.client('datasync', region_name='us-east-1')
 
-def lambda_handler(event,context):
-    response = client.start_task_execution(
-        TaskArn='arn:aws:datasync:us-east-1:135274860544:task/task-0c6a234345cc94d5f',
-        Includes=[
-            {
-                'FilterType': 'SIMPLE_PATTERN',
-                'Value': '/4.50.0'
-            },
-        ]
-    )
+# def lambda_handler(event,context):
+#     response = client.start_task_execution(
+#         TaskArn='arn:aws:datasync:us-east-1:135274860544:task/task-0c6a234345cc94d5f',
+#         Includes=[
+#             {
+#                 'FilterType': 'SIMPLE_PATTERN',
+#                 'Value': '/4.50.0'
+#             },
+#         ]
+#     )
+
+print (sys.argv[0])
