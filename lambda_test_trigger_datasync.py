b82e0599482b838a0c360c48b139aa9a256a222f
aforozco13 <orozco.andres.f.1213@gmail.com>
2023-04-20 22:13:10 -0500

    add environment os 2

--- lambda_test_trigger_datasync.py
+++ lambda_test_trigger_datasync.py
@@ -1,17 +1,18 @@
-#import boto3
+import boto3
 import os
 
-#client = boto3.client('datasync', region_name='us-east-1')
+version = os.environ['Version']
+client = boto3.client('datasync', region_name='us-east-1')
 
-# def lambda_handler(event,context):
-#     response = client.start_task_execution(
-#         TaskArn='arn:aws:datasync:us-east-1:135274860544:task/task-0c6a234345cc94d5f',
-#         Includes=[
-#             {
-#                 'FilterType': 'SIMPLE_PATTERN',
-#                 'Value': '/4.50.0'
-#             },
-#         ]
-#     )
+def lambda_handler(event,context):
+    response = client.start_task_execution(
+        TaskArn='arn:aws:datasync:us-east-1:135274860544:task/task-0c6a234345cc94d5f',
+        Includes=[
+            {
+                'FilterType': 'SIMPLE_PATTERN',
+                'Value': '/{}'.format(version)
+            },
+        ]
+    )
 
-print (os.environ['Version'])
+#print (os.environ['Version'])
