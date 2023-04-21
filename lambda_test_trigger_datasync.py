ef966c58b1d72c3bf9c0e13eaf280cf6f74e8471
aforozco13 <orozco.andres.f.1213@gmail.com>
2023-04-20 21:00:26 -0500

    Mi primer commit

---
+++ lambda_test_trigger_datasync.py
@@ -0,0 +1,14 @@
+import boto3
+
+client = boto3.client('datasync', region_name='us-east-1')
+
+def lambda_handler(event,context):
+    response = client.start_task_execution(
+        TaskArn='arn:aws:datasync:us-east-1:135274860544:task/task-0c6a234345cc94d5f',
+        Includes=[
+            {
+                'FilterType': 'SIMPLE_PATTERN',
+                'Value': '/4.50.0'
+            },
+        ]
+    )
